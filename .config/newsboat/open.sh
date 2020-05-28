40a802bafcad56f14530ce93fb3bc60b1382c780
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2020-05-28 07:48:48 +0300

    Make open.sh work on the background

--- .config/newsboat/open.sh
+++ .config/newsboat/open.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-feh -Z "$1" || mpv --ytdl-format="best[height<=1080]" "$1" || xdg-open "$1"
+(
+    feh -Z "$1" || mpv --ytdl-format="best[height<=1080]" "$1" || xdg-open "$1"
+) &> /dev/null &
