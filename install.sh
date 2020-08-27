55ec6b6127ab3be775cb3e9f87a602f976d55cb9
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2020-08-27 16:25:40 +0300

    Add betterlockscreen

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --needed -S $(cat aur.txt)
-stow -R alacritty bspwm dunst gtk mpd neovim newsboat picom polybar qutebrowser redshift rofi zsh
+stow -R alacritty bspwm dunst gtk mpd neovim newsboat picom polybar qutebrowser redshift rofi zsh betterlockscreen
 betterlockscreen -u $HOME/.config/bspwm/wallpaper.png
