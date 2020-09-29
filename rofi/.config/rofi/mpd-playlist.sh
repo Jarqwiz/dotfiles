0dcbcc79839744e42abbca228ebdc5e5b9dc43a2
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2020-09-29 15:45:36 +0300

    Add mpd commands

---
+++ rofi/.config/rofi/mpd-playlist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+playlists="$(mpc lsplaylists)"
+
+if [ "$playlists" = "" ]; then
+    notify-send "MPD 蘿" "No playlists available"
+    exit 1
+fi
+
+chosen="$(echo -e "$playlists" | rofi -i -dmenu -p "Change playlist 蘿 ")"
+if [ "$chosen" != "" ]; then
+    mpc clear
+    mpc load "$chosen"
+    mpc play
+fi
