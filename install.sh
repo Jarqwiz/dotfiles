c0af44b6237c0009c97908843fbb486cfba56328
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2020-12-05 19:48:36 +0200

    Change wallpaper

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --needed -S $(cat au
 /opt/oomox/plugins/theme_oomox/gtk-theme/change_color.sh -o afterglow "$PWD/afterglow.oomox"
 /opt/oomox/plugins/icons_papirus/change_color.sh -o afterglow "$PWD/afterglow.oomox"
 stow -R xinit alacritty bspwm dunst gtk mpd neovim newsboat picom redshift rofi zsh betterlockscreen polybar
-betterlockscreen -u $HOME/.config/bspwm/wallpaper.jpg -b 1.4
+betterlockscreen -u $HOME/.config/bspwm/wallpaper.png -b 1.4
