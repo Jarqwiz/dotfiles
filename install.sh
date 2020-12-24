091c66518b43e058bf8edbea09a3b695eaab2384
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2020-12-25 00:22:07 +0200

    Late night mpd removal

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --needed -S $(cat aur.txt)
 /opt/oomox/plugins/theme_oomox/gtk-theme/change_color.sh -o afterglow "$PWD/afterglow.oomox"
 /opt/oomox/plugins/icons_papirus/change_color.sh -o afterglow "$PWD/afterglow.oomox"
-stow -R xinit alacritty bspwm dunst gtk mpd neovim newsboat picom redshift rofi zsh betterlockscreen polybar
+stow -R xinit alacritty bspwm dunst gtk neovim picom redshift rofi zsh betterlockscreen polybar
 betterlockscreen -u $HOME/.config/bspwm/wallpaper.png -b 1.4
