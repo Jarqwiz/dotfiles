1205996b655a8cd3874f9a567924c6a6788c74a6
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2021-01-14 14:06:50 +0200

    Make current song better

--- polybar/.config/polybar/current_song.sh
+++ polybar/.config/polybar/current_song.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-TITLE=$(playerctl metadata title 2>/dev/null || echo "No title")
-ARTIST=$(playerctl metadata artist 2>/dev/null || echo "No artist")
-ALBUM=$(playerctl metadata album 2>/dev/null || echo "No album")
-echo " $TITLE ﴁ $ARTIST  $ALBUM"
+TITLE=$(playerctl metadata title 2>/dev/null)
+ARTIST=$(playerctl metadata artist 2>/dev/null)
+ALBUM=$(playerctl metadata album 2>/dev/null)
+
+output=()
+
+if [ -n "$TITLE" ]; then
+	output+=(" $TITLE")
+fi
+
+if [ -n "$ARTIST" ]; then
+	output+=("ﴁ $ARTIST")
+fi
+
+if [ -n "$ALBUM" ]; then
+	output+=(" $ALBUM")
+fi
+
+echo "${output[*]}"
