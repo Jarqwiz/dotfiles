3bf2dcf4987f12e0b235b9b75ac24fbc140fd0d1
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2021-02-05 09:18:15 +0200

    Remove xinit

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --needed -S $(cat aur.txt)
 /opt/oomox/plugins/theme_oomox/gtk-theme/change_color.sh -o afterglow "$PWD/afterglow.oomox"
 /opt/oomox/plugins/icons_papirus/change_color.sh -o afterglow "$PWD/afterglow.oomox"
-stow -R xinit alacritty bspwm dunst gtk neovim picom redshift rofi zsh polybar
+stow -R alacritty bspwm dunst gtk neovim picom redshift rofi zsh polybar
