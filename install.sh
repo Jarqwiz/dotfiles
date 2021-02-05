908a7241781ea79a6a7891e88f95c99dda447f6a
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2021-02-05 10:35:28 +0200

    Remove oomox

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --needed -S $(cat aur.txt)
-/opt/oomox/plugins/theme_oomox/gtk-theme/change_color.sh -o afterglow "$PWD/afterglow.oomox"
-/opt/oomox/plugins/icons_papirus/change_color.sh -o afterglow "$PWD/afterglow.oomox"
 stow -R alacritty bspwm dunst gtk neovim picom redshift rofi zsh polybar
