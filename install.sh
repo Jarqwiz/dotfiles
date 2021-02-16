020eb962a21f27f473a0c94e23d28ef1dfd1aac8
Jarkko Kuukkanen <jarkko.kuukkanen@tuta.io>
2021-02-16 21:03:51 +0200

    Improve theme

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 yay --nocleanmenu --nodiffmenu --noeditmenu --noupgrademenu --needed -S $(cat aur.txt)
+/opt/oomox/plugins/theme_oomox/gtk-theme/change_color.sh -o moonfly "$PWD/moonfly.oomox"
+/opt/oomox/plugins/icons_papirus/change_color.sh -o moonfly "$PWD/moonfly.oomox"
 stow -R alacritty bspwm dunst gtk neovim picom redshift rofi zsh polybar
